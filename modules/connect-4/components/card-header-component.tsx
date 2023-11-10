38511b75863af9646a8b9f79f172b6a0fc5eed95
Lisandro <salvareschilisandro0@gmail.com>
2023-11-10 19:18:23 -0300

    Add CardHeader component with ItemPoint subcomponent

---
+++ modules/connect-4/components/card-header-component.tsx
@@ -0,0 +1,20 @@
+const ItemPoint = ({ color }: { color: string }) => {
+    if (color === "pink") return (
+        <span className={`block w-[20px] h-[20px] bg-c4-pink rounded-full border-2 border-solid border-c4 shadow-c4-default`} />
+    )
+
+    return (
+        <span className={`block w-[20px] h-[20px] bg-c4-yellow rounded-full border-2 border-solid border-c4 shadow-c4-default`} />
+    )
+}
+
+const CardHeader = () => (
+    <div className="w-[max-content] h-[max-content] m-auto grid grid-cols-2 grid-rows-2 gap-1">
+        <ItemPoint color="pink" />
+        <ItemPoint color="yellow" />
+        <ItemPoint color="yellow" />
+        <ItemPoint color="pink" />
+    </div>
+)
+
+export default CardHeader;
