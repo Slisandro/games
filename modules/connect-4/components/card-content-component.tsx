c09836e109f6b2c6d70cba3d757c8840e92924f4
Lisandro <salvareschilisandro0@gmail.com>
2023-11-12 13:32:41 -0300

    Add navigation to play Connect 4

--- modules/connect-4/components/card-content-component.tsx
+++ modules/connect-4/components/card-content-component.tsx
@@ -1,16 +1,20 @@
+"use client"
 import { InfoIcon } from "../assets/icons/info-icon"
 import { PersonIcon } from "../assets/icons/person-icon"
 import { RobotIcon } from "../assets/icons/robot-icon"
+import { useRouter } from 'next/navigation';
 
 const CardContent = () => {
+    const router = useRouter();
+    const navigateToPlay = () => router.push("/connect-4/play");
     return (
         <div className="w-full px-8 flex flex-col items-center gap-8">
-            <button className="uppercase bg-c4-pink text-white font-black px-8 shadow-c4-button rounded-xl py-2 border-solid border-2 border-black text-xl w-full flex justify-between items-center">
+            <button onClick={navigateToPlay} className="uppercase bg-c4-pink text-white font-black px-8 shadow-c4-button rounded-xl py-2 border-solid border-2 border-black text-xl w-full flex justify-between items-center">
                 Play vs CPU
                 <RobotIcon w={50} h={50} />
             </button>
 
-            <button className="uppercase bg-c4-yellow text-black font-black px-8 shadow-c4-button rounded-xl py-2 border-solid border-2 border-black text-xl w-full flex justify-between items-center">
+            <button onClick={navigateToPlay} className="uppercase bg-c4-yellow text-black font-black px-8 shadow-c4-button rounded-xl py-2 border-solid border-2 border-black text-xl w-full flex justify-between items-center">
                 Play vs Player
                 <PersonIcon w={50} h={50} />
             </button>
