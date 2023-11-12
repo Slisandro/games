b0613589b79ee69ce0de260a89ff86ee7e9be337
Lisandro <salvareschilisandro0@gmail.com>
2023-11-12 13:33:04 -0300

    Add Connect-4 game page component.

---
+++ app/connect-4/play/page.tsx
@@ -0,0 +1,10 @@
+import BoardLayouts from '@/modules/connect-4/layouts/board-layouts'
+import React from 'react'
+
+function Page() {
+    return (
+        <BoardLayouts />
+    )
+}
+
+export default Page
