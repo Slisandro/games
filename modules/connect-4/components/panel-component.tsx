7905b005c84a875cca16ac2b468823b5f1e09711
Lisandro <salvareschilisandro0@gmail.com>
2023-11-12 13:31:37 -0300

    Add Connect 4 game board component

---
+++ modules/connect-4/components/panel-component.tsx
@@ -0,0 +1,51 @@
+import { useState } from 'react';
+import TimerComponent from './timer-component';
+import Cell from './cell-component';
+import { checkForWin } from '../utilities/check-for-win-utilities';
+
+function BoardComponent() {
+    const emptyBoard = Array(6).fill(Array(7).fill(null));
+    const [board, setBoard] = useState(emptyBoard);
+    const [currentPlayer, setCurrentPlayer] = useState<"X" | "O">('X');
+
+    const handleClick = (i: number, j: number) => {
+        if (board[i][j] !== null) {
+            return;
+        }
+
+        const newBoard = board.map(row => [...row]); 
+        newBoard[i][j] = currentPlayer;
+        setBoard(newBoard);
+
+        // Check for win
+        if (checkForWin(newBoard, currentPlayer)) {
+            setTimeout(() => {
+                alert(`${currentPlayer} wins!`);
+            }, 1000);
+        }
+
+        setCurrentPlayer(currentPlayer === 'X' ? 'O' : 'X');
+    };
+
+    return (
+        <div className="w-auto h-auto grid grid-rows-6 gap-4 px-4 py-6 bg-white border-4 border-black rounded-xl shadow-c4-card relative">
+            {board.map((row: any, i: number) => (
+                <div className="grid grid-cols-7 gap-4" key={i}>
+                    {row.map((_cell: "X" | "O" | "", j: number) => (
+                        <Cell
+                            key={i + "-" + j}
+                            i={i}
+                            j={j}
+                            currentPlayer={currentPlayer}
+                            handleClick={handleClick}
+                            isSelect={board[i][j]}
+                        />
+                    ))}
+                </div>
+            ))}
+            <TimerComponent currentPlayer={currentPlayer} />
+        </div>
+    )
+}
+
+export default BoardComponent
