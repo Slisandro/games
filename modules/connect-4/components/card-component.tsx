d5ecc3c3224088955e19de3470323032f9574631
Lisandro <salvareschilisandro0@gmail.com>
2023-11-13 16:24:03 -0300

    Refactor Card component to accept className prop.

--- modules/connect-4/components/card-component.tsx
+++ modules/connect-4/components/card-component.tsx
@@ -1,5 +1,9 @@
-const Card = ({ children }: { children: React.ReactNode }) => (
-    <div className="w-1/2 h-auto p-12 bg-c4-purple border-2 border-solid border-c4 rounded-2xl shadow-c4-card flex flex-col gap-12">
+interface Props {
+    children: React.ReactNode,
+    className?: string,
+}
+const Card = ({ children, ...props }: Props) => (
+    <div {...props}>
         {children}
     </div>
 )
