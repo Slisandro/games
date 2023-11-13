9062fc51cb2a750b629770b48155895434f38f82
Lisandro <salvareschilisandro0@gmail.com>
2023-11-13 16:24:57 -0300

    Add Connect 4 rules page component.

---
+++ app/connect-4/rules/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import RulesLayout from '@/modules/connect-4/layouts/rules-layout';
+
+function Page() {
+    return (
+        <RulesLayout />
+    )
+}
+
+export default Page
