16374b5e0ab71f6ef2a512191e1ebd59410ffa2d
Lisandro <salvareschilisandro0@gmail.com>
2023-11-15 22:22:16 -0300

    Add Connect 4 layout component with metadata

---
+++ app/rock-papel-scissors/layout.tsx
@@ -0,0 +1,21 @@
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Connect 4',
+  description: 'Connect four game',
+}
+
+export default function Connect4Layout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  )
+}
