5373b22315f2db13fd88039d47a279fa8d949525
Lisandro <salvareschilisandro0@gmail.com>
2024-11-16 20:14:02 -0300

    changes in cell component

--- modules/connect-4/components/cell-component.tsx
+++ modules/connect-4/components/cell-component.tsx
@@ -9,10 +9,9 @@ interface CellProps {
 }
 
 const Cell: React.FC<CellProps> = ({ i, j, handleClick, isSelect }) => {
-    // console.log(i,j, isSelect)
     return (
         <button
-            className={`grid-cell rounded-full w-14 h-14 border border-4 border-black shadow-t m-auto shadow-c4-board-item ${isSelect === "O" ? "bg-c4-pink" : isSelect === "X" ? "bg-c4-yellow" : "bg-c4"}`}
+            className={`grid-cell rounded-full w-14 h-14 border border-4 border-black shadow-t m-auto shadow-c4-board-item ${isSelect === "X" ? "bg-c4-pink" : isSelect === "O" ? "bg-c4-yellow" : "bg-c4"}`}
             key={`${i}-${j}`}
             onClick={() => handleClick(i, j)}
         >
